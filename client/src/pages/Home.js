ea20fd83e338fd3ff644747915e59479703142cc
AdibAhmed317 <ahmedadib317@gmail.com>
2022-12-30 21:06:43 +0600

    carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
+import Slider2 from '../components/Slider2';
 
 const Home = () => {
   return (
