e260b00a0886da95320a6652ff25face085df414
AdibAhmed317 <ahmedadib317@gmail.com>
2023-08-24 22:55:06 +0600

    global style class

--- client/src/components/Navbar/DropDown.jsx
+++ client/src/components/Navbar/DropDown.jsx
@@ -3,27 +3,52 @@ import { Link } from 'react-router-dom';
 import NavbarContext from '../../context/NavbarContext';
 
 const Dropdown = () => {
-  const { toggle, isOpen } = useContext(NavbarContext);
-  //   console.log(toggle, isOpen);
+  const { isOpen } = useContext(NavbarContext);
   return (
     <div
       className={
         isOpen
-          ? 'grid grid-cols-1 text-center items-center bg-green-300 text-green-900 w-full h-full'
+          ? 'grid grid-cols-1 text-center items-center bg-green-300 text-green-900 w-full h-full p-10'
           : 'hidden'
       }>
-      <Link to='#intro' className='p-4 cursor-pointer'>
-        Home
-      </Link>
-      <Link to='#about' className='p-4 cursor-pointer'>
-        About
-      </Link>
-      <Link to='#projects' className='p-4 cursor-pointer'>
-        Projects
-      </Link>
-      <Link to='#contact' className='p-4 cursor-pointer'>
-        Contact
-      </Link>
+      <ul className='flex flex-col justify-center items-center'>
+        <li className='justify-center items-center flex'>
+          <Link className='small-navlink' to='/'>
+            Home
+          </Link>
+        </li>
+        <li className='justify-center items-center flex'>
+          <Link className='small-navlink' to='/'>
+            Shop
+          </Link>
+        </li>
+        <li className='justify-center items-center flex'>
+          <Link className='small-navlink' to='/'>
+            Contact
+          </Link>
+        </li>
+        <li className='justify-center items-center flex'>
+          <Link className='small-navlink' to='/'>
+            About Us
+          </Link>
+        </li>
+      </ul>
+      <ul className='flex justify-between items-center'>
+        <li>
+          <Link
+            className='my-1 text-base text-green-50 font-medium m-0 bg-blue-600 hover:bg-blue-900 py-2 px-3 md:mx-2 rounded-md'
+            to='/login'>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            className='my-1 text-base text-green-50 font-medium m-0 bg-purple-700 hover:bg-purple-500 py-2 px-3 md:mx-2 rounded-md'
+            to='/login'>
+            Signup
+          </Link>
+        </li>
+      </ul>{' '}
     </div>
   );
 };
--- client/src/pages/Client/HomePage.jsx
+++ client/src/pages/Client/HomePage.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Hero from '../../components/Hero Section/Hero';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
-import Demo from '../../components/Navbar/demo';
 import Dropdown from '../../components/Navbar/DropDown';
 
 const HomePage = ({ Name }) => {
   return (
     <div className='bg-green-50'>
-      <Demo />
+      <Navbar />
       <Dropdown />
       <Hero />
       <Footer />
