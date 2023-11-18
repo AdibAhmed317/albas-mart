dcd41a1ace8c7d39a5c9c64480b559a7c02666ea
AdibAhmed317 <ahmedadib317@gmail.com>
2023-11-19 03:02:06 +0600

    product image

--- client/src/components/Cart/CartList.jsx
+++ client/src/components/Cart/CartList.jsx
@@ -9,7 +9,10 @@ const CartList = ({ product }) => {
           <>
             <hr />
             <div key={product._id} className='flex flex-col md:flex-row m-5'>
-              <img src={dp} className='h-[150px] w-[150px] object-cover' />
+              <img
+                src={product.img}
+                className='h-[150px] w-[150px] object-cover'
+              />
               <div className='ml-0 md:ml-5'>
                 <h1 className='text-3xl font-serif m-2'>{product.title}</h1>
                 <p className='text-lg m-2'>Price: ${product.price}</p>
