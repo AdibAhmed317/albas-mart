f111b7cb20acc0755b856d0dcb0a9f2a282cb5c7
AdibAhmed317 <ahmedadib317@gmail.com>
2024-04-01 03:01:40 +0600

    firebase environment variable added (client)

--- apps/client/src/firebase.js
+++ apps/client/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5_Asde5tDoJyHTBuvDz2UXpvdSgRr9T8',
-  authDomain: 'al-raya-e65bc.firebaseapp.com',
-  projectId: 'al-raya-e65bc',
-  storageBucket: 'al-raya-e65bc.appspot.com',
-  messagingSenderId: '758319124716',
-  appId: '1:758319124716:web:643bdd06d12655693995c9',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
