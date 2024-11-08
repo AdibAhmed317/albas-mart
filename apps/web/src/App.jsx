7d148f859dc624f78d3e5cd1447c930bc0ab6880
AdibAhmed <ahmedadib317@gmail.com>
2024-11-09 00:30:41 +0600

    added kebab-case

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import SignIn from '@/pages/auth/sign-in.jsx';
-import SignUp from '@/pages/auth/sign-up.jsx';
+import SignIn from '@/pages/auth/sign-in';
+import SignUp from '@/pages/auth/sign-up';
 
-// import AdminDashboard from '@/pages/admin/AdminDashboard.jsx';
-// import AllOrders from '@/pages/admin/AllOrders.jsx';
-// import AdminProfile from '@/pages/admin/AdminProfile.jsx';
+import AdminDashboard from '@/pages/admin/admin-dashboard';
+import AllOrders from '@/pages/admin/all-orders';
+import AdminProfile from '@/pages/admin/admin-profile';
 
 import HomePage from '@/pages/client/home-page';
-// import SingleProduct from '@/pages/client/SingleProduct.jsx';
-// import UserProfile from '@/pages/client/profile/UserProfile.jsx';
-// import About from '@/pages/client/About.jsx';
-// import Shop from '@/pages/client/Shop.jsx';
-// import Cart from '@/pages/client/Cart.jsx';
-// import Contact from '@/pages/client/Contact.jsx';
-// import AdminProduct from '@/pages/admin/AdminProduct.jsx';
+import SingleProduct from '@/pages/client/single-product';
+import UserProfile from '@/pages/client/profile/user-profile';
+import About from '@/pages/client/about';
+import Shop from '@/pages/client/shop';
+import Cart from '@/pages/client/cart';
+import Contact from '@/pages/client/contact';
+import AdminProduct from '@/pages/admin/admin-dashboard';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        {/* <Route path='/shop/:category' element={<Shop />} />
+        <Route path='/shop/:category' element={<Shop />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/about' element={<About />} />
         <Route path='/cart' element={<Cart />} />
-        <Route path='/product/:id' element={<SingleProduct />} /> */}
+        <Route path='/product/:id' element={<SingleProduct />} />
 
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-        {/* <Route path='/user-profile/:id' element={<UserProfile />} />
+        <Route path='/user-profile/:id' element={<UserProfile />} />
 
         <Route path='/admin-dashboard/:id' element={<AdminDashboard />} />
         <Route path='/admin-profile/:id' element={<AdminProfile />} />
         <Route path='/admin/create-product/:id' element={<AdminProduct />} />
-        <Route path='/admin/all-orders/:id' element={<AllOrders />} /> */}
+        <Route path='/admin/all-orders/:id' element={<AllOrders />} />
       </Routes>
     </>
   );
