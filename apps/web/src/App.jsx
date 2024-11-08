dd4c5750116db5e1ad904a28352457a669b7a8f1
AdibAhmed <ahmedadib317@gmail.com>
2024-11-09 00:04:54 +0600

    extension added in import

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import SignIn from '@/pages/auth/SignIn.jsx';
-import SignUp from '@/pages/auth/SignUp.jsx';
+// import SignIn from '@/pages/auth/SignIn.jsx';
+// import SignUp from '@/pages/auth/SignUp.jsx';
 
-import AdminDashboard from '@/pages/admin/AdminDashboard.jsx';
-import AllOrders from '@/pages/admin/AllOrders.jsx';
-import AdminProfile from '@/pages/admin/AdminProfile.jsx';
+// import AdminDashboard from '@/pages/admin/AdminDashboard.jsx';
+// import AllOrders from '@/pages/admin/AllOrders.jsx';
+// import AdminProfile from '@/pages/admin/AdminProfile.jsx';
 
 import HomePage from '@/pages/client/HomePage.jsx';
-import SingleProduct from '@/pages/client/SingleProduct.jsx';
-import UserProfile from '@/pages/client/profile/UserProfile.jsx';
-import About from '@/pages/client/About.jsx';
-import Shop from '@/pages/client/Shop.jsx';
-import Cart from '@/pages/client/Cart.jsx';
-import Contact from '@/pages/client/Contact.jsx';
-import AdminProduct from '@/pages/admin/AdminProduct.jsx';
+// import SingleProduct from '@/pages/client/SingleProduct.jsx';
+// import UserProfile from '@/pages/client/profile/UserProfile.jsx';
+// import About from '@/pages/client/About.jsx';
+// import Shop from '@/pages/client/Shop.jsx';
+// import Cart from '@/pages/client/Cart.jsx';
+// import Contact from '@/pages/client/Contact.jsx';
+// import AdminProduct from '@/pages/admin/AdminProduct.jsx';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/shop/:category' element={<Shop />} />
+        {/* <Route path='/shop/:category' element={<Shop />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/about' element={<About />} />
         <Route path='/cart' element={<Cart />} />
@@ -35,7 +35,7 @@ function App() {
         <Route path='/admin-dashboard/:id' element={<AdminDashboard />} />
         <Route path='/admin-profile/:id' element={<AdminProfile />} />
         <Route path='/admin/create-product/:id' element={<AdminProduct />} />
-        <Route path='/admin/all-orders/:id' element={<AllOrders />} />
+        <Route path='/admin/all-orders/:id' element={<AllOrders />} /> */}
       </Routes>
     </>
   );
